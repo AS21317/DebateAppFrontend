4eeeaee526e41c164a8af582f896b65aa64b5309
AS21317 <ashish3553singh@gmail.com>
2024-05-07 23:17:05 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,32 +1,32 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
-
-
-
-// vite.config.js
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: '0.0.0.0',
-    port: 5173,
-    proxy: {
-      '/service-worker.js': {
-        target: 'http://127.0.0.1:5173', // Use IPv4 loopback address
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
-  base: './',
-  mode: 'development',
-});
+})
+
+
+
+
+// // vite.config.js
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     host: '0.0.0.0',
+//     port: 5173,
+//     proxy: {
+//       '/service-worker.js': {
+//         target: 'http://127.0.0.1:5173', // Use IPv4 loopback address
+//         changeOrigin: true,
+//         secure: false,
+//       },
+//     },
+//   },
+//   base: './',
+//   mode: 'development',
+// });
